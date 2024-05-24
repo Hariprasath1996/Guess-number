a75e8c549fc152981359c4dbc94bc77867c9d6ab
HariPrasath1996 <hasathphy96@gmail.com>
2024-05-24 17:29:51 +0530

    create math method

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-// select button and para tag 
-let btn=document.getElementById("btn")
-let score = document.getElementById("score")
+// select input tag  and para tag 
+let guessNumber=document.getElementById("guessNumber")
+let result = document.getElementById("score")
+// create randomNumber variable  (its created for which number are you typing in input tag)
+let randomNumber = math.floor(math.random()*10)+1
 
 
